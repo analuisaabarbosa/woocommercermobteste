835bfb58d62c4e292c35932fdee3655ea0f5dd1c
Ana <analuisaaugustob@gmail.com>
2024-01-24 12:49:54 -0300

    Configurando arquivo

--- wdio.conf.js
+++ wdio.conf.js
@@ -9,12 +9,12 @@ exports.config = {
     ],
     framework: 'mocha',
     capabilities: [{
-        "platformName": "Android",
-        "platformVersion": "11.0",
-        "deviceName": "nightwatch-android-11",
-        "automationName": "UiAutomator2",
-        "app": join(process.cwd(), './app/wcandroid-14.0.apk'),
-        "appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity',
+        "appium:platformName": "Android",
+        "appium:platformVersion": "11.0",
+        "appium:deviceName": "nightwatch-android-11",
+        "appium:automationName": "UiAutomator2",
+        "appium:app": join(process.cwd(), './app/wcandroid-14.0.apk'),
+        "appium:appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity',
     }],
     services: [
         ['appium', {
@@ -22,7 +22,6 @@ exports.config = {
                 address: 'localhost',
                 port: 4723
             },
-            logPath:'./',
         }]
     ],
 }
